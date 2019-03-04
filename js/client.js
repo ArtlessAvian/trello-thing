0ba1b225b3d6622dbab228d11ff883c693cf4300
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-03-04 14:45:42 -0800

    yup

--- js/client.js
+++ js/client.js
@@ -3,16 +3,21 @@ alert("Hello world!")
 var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
+var onRecalculateBtn = function(t, opts) {
+    console.log(t.lists("all"))
+}
+
+t_test = TrelloPowerUp.iframe()
+
 TrelloPowerUp.initialize({
     'board-buttons': function (t, opts) {
         return [{
-            // we can either provide a button that has a callback function
             icon: {
                 dark: WHITE_ICON,
                 light: BLACK_ICON
             },
             text: 'Recalculate',
-            callback: alert,
+            callback: onRecalculateBtn,
             condition: 'edit'
         }];
     }
