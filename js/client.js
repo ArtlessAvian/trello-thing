6284f36f2bd0dd6b12cd432186c74ed7e743752a
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-03-04 14:37:01 -0800

    i cant really test this without pushing it to the pages

--- js/client.js
+++ js/client.js
@@ -1,1 +1,19 @@
 alert("Hello world!")
+
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+TrelloPowerUp.initialize({
+    'board-buttons': function (t, opts) {
+        return [{
+            // we can either provide a button that has a callback function
+            icon: {
+                dark: WHITE_ICON,
+                light: BLACK_ICON
+            },
+            text: 'Recalculate',
+            callback: alert,
+            condition: 'edit'
+        }];
+    }
+});
