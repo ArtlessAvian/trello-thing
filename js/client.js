3a0dd9da3ea3d9ff8a5e585b77a2937ac962c500
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-03-04 14:54:19 -0800

    ;(

--- js/client.js
+++ js/client.js
@@ -3,8 +3,8 @@ alert("Hello world!")
 var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
-var onRecalculateBtn = function(t, opts) {
-    console.log("someone clicked the button")
+var onRecalculateBtn = function(t) {
+    console.log("haha yes please work")
 }
 
 TrelloPowerUp.initialize({
