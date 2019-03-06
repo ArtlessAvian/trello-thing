14f0951142d64d717ac1755d6098b616b5a5de6e
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-03-05 16:36:03 -0800

    whoops not global

--- js/client.js
+++ js/client.js
@@ -3,6 +3,8 @@ alert("Hello world!")
 var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
+var t_test = null
+
 var onRecalculateBtn = function(t) {
     t_test = t
 }
