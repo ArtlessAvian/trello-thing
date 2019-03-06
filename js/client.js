bc7e1ff10c56b3339b34e8320ade7d8720d5d750
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-03-05 19:03:55 -0800

    "Working" with naive velocity calculation.
    The velocity should be the same every day, if work is completed at that speed
    every day. At least, the variance between days should be minimized.
    
    It's working at least, but it telling me I need to do ~4 points per day.
    That's a small project or week of small homework. Yeesh.

--- js/client.js
+++ js/client.js
@@ -1,21 +1,65 @@
-alert("Hello world!")
+// alert("Hello world!")
 
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+var pointsRegex = RegExp("(\\d+) Points?")
 
-var t_test = null
+var targetVelocity = -1
+var calcVeclocity = function(lists) {
+    targetVelocity = 0
+    for (let listIndex in lists) {
+        let list = lists[listIndex];
+        
+        for (let cardIndex in list.cards) {
+            let card = list.cards[cardIndex];
+            if (card.dueComplete) {continue;}
+
+            let points = 0;
+
+            // Get points from label
+            for (let labelIndex in card.labels) {
+                let label = card.labels[labelIndex];
+
+                let labelMatches = pointsRegex.exec(label.name);
+                if (labelMatches != null && labelMatches.length == 2) {
+                    points += Number.parseInt(labelMatches[1]);
+                    break;
+                }
+                console.log("im still gay")
+            }
+
+            // Subtract completed points
+            let cardMatches = pointsRegex.exec(card.desc);
+            if (cardMatches != null && cardMatches.length == 2) {
+                points -= Number.parseInt(cardMatches[1]);
+            }
+
+            // Add to velocity
+            if (points != 0) {
+                let dueDate = new Date(Date.parse(card.due)-1); // -1 for midnight due dates
+                let nowDate = new Date(Date.now()-1); // -1 for relative
+                
+                // dueDate.setMilliseconds(0); dueDate.setSeconds(0); dueDate.setMinutes(0); dueDate.setHours(0);
+                // nowDate.setMilliseconds(0); nowDate.setSeconds(0); nowDate.setMinutes(0); nowDate.setHours(0);
+                
+                let days = (dueDate - nowDate) / 86400000;
+                console.log(points, days, card.name);
+                // console.log(points/days, card.name);
+                targetVelocity += points/days;
+            }
+        }
+    }
+}
 
 var onRecalculateBtn = function(t) {
-    t_test = t
+    t.lists('cards')
+    .then(calcVeclocity)
+    .then(function() {
+        alert(targetVelocity)
+    });
 }
 
 TrelloPowerUp.initialize({
     'board-buttons': function (t, opts) {
         return [{
-            icon: {
-                dark: WHITE_ICON,
-                light: BLACK_ICON
-            },
             text: 'Recalculate',
             callback: onRecalculateBtn,
             condition: 'edit'
