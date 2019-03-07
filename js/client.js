8666efb40f74c05b8b1429d773c88daec4d933b4
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-03-06 17:08:49 -0800

    Seperate Scripts

--- js/client.js
+++ js/client.js
@@ -3,59 +3,51 @@
 var pointsRegex = RegExp("(\\d+) Points?")
 
 var targetVelocity = -1
-var calcVeclocity = function(lists) {
-    targetVelocity = 0
-    for (let listIndex in lists) {
-        let list = lists[listIndex];
+// var calcVeclocity = function(lists) {
+    // targetVelocity = 0
+    // for (let listIndex in lists) {
+    //     let list = lists[listIndex];
         
-        for (let cardIndex in list.cards) {
-            let card = list.cards[cardIndex];
-            if (card.dueComplete) {continue;}
+    //     for (let cardIndex in list.cards) {
+    //         let card = list.cards[cardIndex];
+    //         if (card.dueComplete) {continue;}
 
-            let points = 0;
+    //         let points = 0;
 
-            // Get points from label
-            for (let labelIndex in card.labels) {
-                let label = card.labels[labelIndex];
+    //         // Get points from label
+    //         for (let labelIndex in card.labels) {
+    //             let label = card.labels[labelIndex];
 
-                let labelMatches = pointsRegex.exec(label.name);
-                if (labelMatches != null && labelMatches.length == 2) {
-                    points += Number.parseInt(labelMatches[1]);
-                    break;
-                }
-                console.log("im still gay")
-            }
+    //             let labelMatches = pointsRegex.exec(label.name);
+    //             if (labelMatches != null && labelMatches.length == 2) {
+    //                 points += Number.parseInt(labelMatches[1]);
+    //                 break;
+    //             }
+    //             console.log("im still gay")
+    //         }
 
-            // Subtract completed points
-            let cardMatches = pointsRegex.exec(card.desc);
-            if (cardMatches != null && cardMatches.length == 2) {
-                points -= Number.parseInt(cardMatches[1]);
-            }
+    //         // Subtract completed points
+    //         let cardMatches = pointsRegex.exec(card.desc);
+    //         if (cardMatches != null && cardMatches.length == 2) {
+    //             points -= Number.parseInt(cardMatches[1]);
+    //         }
 
-            // Add to velocity
-            if (points != 0) {
-                let dueDate = new Date(Date.parse(card.due)-1); // -1 for midnight due dates
-                let nowDate = new Date(Date.now()-1); // -1 for relative
+    //         // Add to velocity
+    //         if (points != 0) {
+    //             let dueDate = new Date(Date.parse(card.due)-1); // -1 for midnight due dates
+    //             let nowDate = new Date(Date.now()-1); // -1 for relative
                 
-                // dueDate.setMilliseconds(0); dueDate.setSeconds(0); dueDate.setMinutes(0); dueDate.setHours(0);
-                // nowDate.setMilliseconds(0); nowDate.setSeconds(0); nowDate.setMinutes(0); nowDate.setHours(0);
+    //             // dueDate.setMilliseconds(0); dueDate.setSeconds(0); dueDate.setMinutes(0); dueDate.setHours(0);
+    //             // nowDate.setMilliseconds(0); nowDate.setSeconds(0); nowDate.setMinutes(0); nowDate.setHours(0);
                 
-                let days = (dueDate - nowDate) / 86400000;
-                console.log(points, days, card.name);
-                // console.log(points/days, card.name);
-                targetVelocity += points/days;
-            }
-        }
-    }
-}
-
-var onRecalculateBtn = function(t) {
-    t.lists('cards')
-    .then(calcVeclocity)
-    .then(function() {
-        alert(targetVelocity)
-    });
-}
+    //             let days = (dueDate - nowDate) / 86400000;
+    //             console.log(points, days, card.name);
+    //             // console.log(points/days, card.name);
+    //             targetVelocity += points/days;
+    //         }
+    //     }
+    // }
+// }
 
 TrelloPowerUp.initialize({
     'board-buttons': function (t, opts) {
