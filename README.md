368171f82918fe1dd5861682ffa549169637622e
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-03-06 19:11:16 -0800

    Good Algorithm.
    Need to work on the presentation though.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Story Points Based Automation of Trello
-Just poking around.
+**(AKA Yet Another Redundant Productivity Tool)**
+
+A Trello Powerup to determine how much work to do each day.
+
+Enough to meet each deadline, but
+not so much as to overwork yourself.
+
+## Deployment
+1) Host an http server serving this repository
+    * Easily done with `python -m http.server --bind 127.0.0.1`
+    * This will probably be unnecesary once testing is actually done.
+2) Make a powerup for a team
+    * Enable the `callbacks` and `board-buttons` capabilities.
+    * Set the "Iframe connector URL" to the Github Page for this repository.
+3) Add to a Trello board.
+
+## Usage
+1) *Create categories* with labels with names containing `### Points`.
+2) *List tasks* with cards.
+3) *Estimate work* for cards using labels and due dates.
+4) *Calculate velocity* by hitting the calculate button on the board.
+5) *Mark completed points* by adding `### Points` somewhere in the description.
--- js/recalculate.js
+++ js/recalculate.js
@@ -1,54 +1,100 @@
-var targetVelocity = -1
-var calcVeclocity = function(lists) {
-    targetVelocity = 0
+var pointsRegex = RegExp("(\\d+) [pP]oints?")
+
+var getPoints = function(card) {
+    let points = 0;
+
+    // Get points from label
+    for (let labelIndex in card.labels) {
+        let label = card.labels[labelIndex];
+
+        let labelMatches = pointsRegex.exec(label.name);
+        if (labelMatches != null && labelMatches.length == 2) {
+            points += Number.parseInt(labelMatches[1]);
+            // break; // Count all labels or no?
+        }
+    }
+
+    // Subtract completed points
+    let cardMatches = pointsRegex.exec(card.desc);
+    if (cardMatches != null && cardMatches.length == 2) {
+        points -= Number.parseInt(cardMatches[1]);
+    }
+
+    return points;
+}
+
+var dateOffset = (new Date().getTimezoneOffset() + 60 * 2) * 60 * 10000 // Timezone offset + 2 Hours for 10-midnight due dates
+var dateTransform = function(milliseconds) {
+    let out = new Date(milliseconds + dateOffset);
+    out.setMilliseconds(0); out.setSeconds(0); out.setMinutes(0); out.setHours(0);
+    return out;
+}
+
+var getPointsByDate = function(lists) {
+    let pointsByDate = []
+    let nowDate = dateTransform(Date.now()); // +1 hour
+
     for (let listIndex in lists) {
         let list = lists[listIndex];
-        
         for (let cardIndex in list.cards) {
             let card = list.cards[cardIndex];
-            if (card.dueComplete) {continue;}
+            if (card.dueComplete) {continue;}   
 
-            let points = 0;
-
-            // Get points from label
-            for (let labelIndex in card.labels) {
-                let label = card.labels[labelIndex];
-
-                let labelMatches = pointsRegex.exec(label.name);
-                if (labelMatches != null && labelMatches.length == 2) {
-                    points += Number.parseInt(labelMatches[1]);
-                    break;
-                }
-                console.log("im still gay")
-            }
-
-            // Subtract completed points
-            let cardMatches = pointsRegex.exec(card.desc);
-            if (cardMatches != null && cardMatches.length == 2) {
-                points -= Number.parseInt(cardMatches[1]);
-            }
-
-            // Add to velocity
+            let points = getPoints(card)
             if (points != 0) {
-                let dueDate = new Date(Date.parse(card.due)-1); // -1 for midnight due dates
-                let nowDate = new Date(Date.now()-1); // -1 for relative
-                
-                // dueDate.setMilliseconds(0); dueDate.setSeconds(0); dueDate.setMinutes(0); dueDate.setHours(0);
-                // nowDate.setMilliseconds(0); nowDate.setSeconds(0); nowDate.setMinutes(0); nowDate.setHours(0);
-                
-                let days = (dueDate - nowDate) / 86400000;
+                let dueDate = dateTransform(Date.parse(card.due));
+                let days = Math.ceil((dueDate - nowDate) / 86400000);
                 console.log(points, days, card.name);
-                // console.log(points/days, card.name);
-                targetVelocity += points/days;
+                while(pointsByDate.length <= days) {pointsByDate.push(0);} // Trailing 0 is intentional
+                pointsByDate[days-1] += points;
             }
         }
     }
+    return pointsByDate;
 }
 
+var refineVelocity = function(pointsByDate) {
+    let previous = 0;
+    let count = 0;
+    let sum = 0;
+    let out = [];
+    for (let index in pointsByDate) {
+        if (pointsByDate[index] < previous) {
+            for (let i = 0; i < count; i++) {out.push(sum/count);}
+            sum = 0;
+            count = 0;
+        }
+        previous = pointsByDate[index];
+        count++;
+        sum += previous;
+    }
+    out.push(0); // Trailing 0 again
+    return out;
+}
+
+// O(n^2) for correctness. O(n) for good enough
 var onRecalculateBtn = function(t) {
     t.lists('cards')
-    .then(calcVeclocity)
-    .then(function() {
-        alert(targetVelocity)
+
+    .then(getPointsByDate)
+    
+    .then(function(pointsByDate) {
+        console.log(pointsByDate)
+        return refineVelocity(pointsByDate);
+    })
+    
+    .then(function(pointsByDate) {
+        console.log(pointsByDate)
+        return refineVelocity(pointsByDate);
+    })
+    
+    .then(function(pointsByDate) {
+        console.log(pointsByDate)
+        return refineVelocity(pointsByDate);
+    })
+    
+    .then(function(pointsByDate) {
+        // Probably good enough
+        console.log(pointsByDate.slice(0, 7))
     });
 }
